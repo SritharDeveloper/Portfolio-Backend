662e3c7fd79a3aaade19da9dd4374fb43d94025f
SritharDeveloper <sritharmoorthy123@gmail.com>
2026-06-11 09:51:49 +0530

    Update CORS middleware to allow all origins
    Simplified CORS middleware configuration to allow all origins.

--- server/index.js
+++ server/index.js
@@ -11,14 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://portfolio-ml8ee2ti1-srithardevelopers-projects.vercel.app",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Routes
